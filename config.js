c692cf3827cd96235969e1487a3c74736472e92f
pansensoyglenn-dev <pansensoyglenn@gmail.com>
2026-04-15 11:56:05 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,7 @@
-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
+const GITHUB_TOKEN = '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';
+
+window.addEventListener('DOMContentLoaded', () => {
+  if (GITHUB_TOKEN && GITHUB_TOKEN !== 'ghp_YOUR_TOKEN_HERE') {
+    localStorage.setItem('pc_gh_token', GITHUB_TOKEN);
+  }
+});
